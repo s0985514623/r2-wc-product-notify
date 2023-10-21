a6da52fbfc15053e03e53c0f729af250cb8fe381
Ren <s0985514623@gmail.com>
2023-10-21 18:41:31 +0800

    feat:課前通知將提前3天通知操作人員&優化代碼

--- inc/admin/settingPage.php
+++ inc/admin/settingPage.php
@@ -24,6 +24,9 @@ function r2_notify_page_content()
 		update_option('r2_notify_days_before', $days_before); // 更新選項
 		$clock_before = sanitize_text_field($_POST['r2_notify_clock_before']); // 獲取表單提交的值
 		update_option('r2_notify_clock_before', $clock_before); // 更新選項
+		//如果有設定操作人員email則取得，否則預設為網站管理員
+		$r2_notify_operator_mail = sanitize_text_field($_POST['r2_notify_operator_mail']);
+		update_option('r2_notify_operator_mail', $r2_notify_operator_mail); // 更新選項
 		echo '<div class="updated"><p>設置已保存。</p></div>';
 	}
 
@@ -33,9 +36,12 @@ function r2_notify_page_content()
 		<h2>課前提醒通知設定</h2>
 		<form method="post" action="">
 			<div class="pageSection">
+				<div><span style="display:block; width: 180px;">操作人員Email(將於發信前三天通知):</span></div>
+				<div><input type="text" name="r2_notify_operator_mail" placeholder="預設為網站管理員" value="<?php echo esc_attr(get_option('r2_notify_operator_mail')); ?>"></div>
+			</div>
+			<div class="pageSection">
 				<div><span style="display:block; width: 180px;">要在到期前幾天通知：</span></div>
 				<div><input type="number" name="r2_notify_days_before" placeholder="預設為1天" value="<?php echo esc_attr(get_option('r2_notify_days_before', 1)); ?>"></div>
-
 			</div>
 			<div class="pageSection">
 				<div><span style="display:block; width: 180px;">要在幾點通知：(24小時制)</span></div>
